32bbf813c35d7f4163ae08beef2a0485addc36ae
zyf220 <739817521@qq.com>
2022-04-11 18:36:19 +0800

    测试提交

---
+++ src/main/java/com/zyf/notebook/configure/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.zyf.notebook.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+
+    }
+}
